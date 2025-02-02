f61b1cb86b7ea4316980ca9b226b25bf2bdd64fd
Developer-Jihad <jihad.developer.bd@gmail.com>
2025-02-03 02:09:14 +0600

    [Fixed] CTA Not Showing Issue

--- components/sections/CTASection.tsx
+++ components/sections/CTASection.tsx
@@ -22,28 +22,29 @@ const useIsLargeScreen = () => {
 };
 
 const CTASection = () => {
-
   const isLargeScreen = useIsLargeScreen();
 
   return (
     <section
-      className={`relative bg-primary h-[350] md:h-[500px] overflow-hidden ${
+      className={`bg-primary h-[350] md:h-[500px] overflow-hidden ${
         isLargeScreen ? "cta-clip-lg" : "cta-clip-sm"
       }`}
     >
-      <div className="absolute flex flex-col w-full h-full items-center justify-center text-center text-white z-20 p-8 mt-4 md:mt-14 ">
-        <h2 className="text-2xl md:text-4xl font-bold mb-[24px]">
-          Let Us Connect
-        </h2>
-        <p className=" md:text-2xl mb-[30px]">
-          Talk to us to find out how we can transform your organisation for the
-          future.
-        </p>
-        <Button />
-      </div>
+      <div className="container relative">
+        <div className="relative lg:absolute flex flex-col w-full h-[350] md:h-[500px] items-center justify-center text-center text-white z-20 p-8 mt-4 md:mt-14 ">
+          <h2 className="text-2xl md:text-4xl font-bold mb-[24px]">
+            Let Us Connect
+          </h2>
+          <p className=" md:text-2xl mb-[30px]">
+            Talk to us to find out how we can transform your organisation for
+            the future.
+          </p>
+          <Button />
+        </div>
 
-      <div className="hidden lg:block relative z-10">
-        <ParallaxBackground />
+        <div className="hidden lg:block relative z-10">
+          <ParallaxBackground />
+        </div>
       </div>
     </section>
   );
